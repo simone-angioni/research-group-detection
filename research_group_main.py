67d9f1071eac5eeef14945941714c23b080513b8
Simone Angioni <87153453+simone-angioni@users.noreply.github.com>
2026-05-07 17:38:49 +0200

    Add files via upload

--- research_group_main.py
+++ research_group_main.py
@@ -1,8 +1,7 @@
 from .research_groups_detection import *
 from .download_from_openaire_api import *
 
-
-def build_network_from_orcid(orcid, progress_callback: Optional[Callable] = None):
+def get_research_products(orcid):
     BASE_URL = "https://api.openaire.eu/graph/v2/researchProducts"
 
     PARAMS = {
@@ -24,10 +23,12 @@ def build_network_from_orcid(orcid, progress_callback: Optional[Callable] = None
 
     ego_coauthors = get_coauthors(products)
     
-    all_works = retrieve_coauthors_works(ego_coauthors, BASE_URL, HEADERS)
-    enrich_works_for_coauthors(all_works, ego_coauthors)
+    research_products = retrieve_coauthors_works(ego_coauthors, BASE_URL, HEADERS)
+    enrich_works_for_coauthors(research_products, ego_coauthors)
+    return research_products
 
-    coauthorships = compute_coauthorships(all_works, ego_coauthors)
+def get_research_groups(research_products):
+    coauthorships = compute_coauthorships(research_products, ego_coauthors)
     network = build_network(coauthorships)
     finder = RecursiveCommunitiesFinder(network, leidenalg.ModularityVertexPartition)
     ego = ego_coauthors[orcid]
@@ -39,17 +40,24 @@ def build_network_from_orcid(orcid, progress_callback: Optional[Callable] = None
 
     threshold = 0.3
 
-    temporal_research_groups, authors, output_works = compute_temporal_research_groups(communities, threshold)
+    temporal_research_groups, authors_mapping, research_products_detailed = compute_temporal_research_groups(communities, threshold)
 
     members = get_members(temporal_research_groups)
-    members = {}
-    for year, values in years.items():
-        members[year] = values['research-group']
     
     coauthors_per_year = get_coauthors_per_year(coauthorships, ego)
     research_groups = assemble_research_groups(members, communities, ego, coauthors_per_year)
 
     graph = {'research-groups-overview':[{'id': i, 'start': g['start'], 'end': g['end'], 'research-group': [authors[m] for m in g['members']]}for i, g in enumerate(research_groups, start=1)],
-		 'years': years}
+		 'years': temporal_research_groups}
 
-    return graph, authors, output_works
+    return graph, authors, research_products_detailed
+
+def main(orcid):
+    research_products = get_research_products(orcid)
+    graph, authors_mapping, research_products_detailed = get_research_groups(research_products)
+    with open(f"{orcid}-rg-network.json", "w") as f:
+        json.dump(graph)
+    with open(f"{orcid}-rg-network-author-names.json", "w") as f:
+        json.dump(authors_mapping)
+    with open(f"{orcid}-rg-network-products-details.json", "w") as f:
+        json.dump(research_products_detailed)
